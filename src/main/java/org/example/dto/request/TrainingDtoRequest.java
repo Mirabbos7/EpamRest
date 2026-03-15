dd878f0a7347151a078cb2988ce18d58d1d7153e
Mirabbos7 <155135153+Mirabbos7@users.noreply.github.com>
2026-03-15 14:59:52 +0500

    added validation annotations

--- src/main/java/org/example/dto/request/TrainingDtoRequest.java
+++ src/main/java/org/example/dto/request/TrainingDtoRequest.java
@@ -1,14 +1,16 @@
 package org.example.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.example.entity.TrainingType;
-
 import java.util.Date;
 
 public record TrainingDtoRequest(
-        String traineeUsername,
-        String trainerUsername,
-        String trainingName,
-        TrainingType.TrainingTypeName typeName,
-        Date trainingDate,
-        int durationMinutes
+        @NotBlank String traineeUsername,
+        @NotBlank String trainerUsername,
+        @NotBlank String trainingName,
+        @NotNull TrainingType.TrainingTypeName typeName,
+        @NotNull Date trainingDate,
+        @Min(1) int durationMinutes
 ) {}
