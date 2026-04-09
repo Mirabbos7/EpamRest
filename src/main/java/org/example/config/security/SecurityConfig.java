41f9a5d44535684c416974dfb062b7da00b2549c
Mirabbos7 <155135153+Mirabbos7@users.noreply.github.com>
2026-04-09 22:28:30 +0500

    location refactored

--- src/main/java/org/example/config/security/SecurityConfig.java
+++ src/main/java/org/example/config/security/SecurityConfig.java
@@ -1,7 +1,7 @@
 package org.example.config.security;
 
 import lombok.RequiredArgsConstructor;
-import org.example.config.filter.JwtAuthenticationFilter;
+import org.example.filter.JwtAuthenticationFilter;
 import org.example.config.service.UserDetailsLoader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/org/example/filter/JwtAuthenticationFilter.java
+++ src/main/java/org/example/filter/JwtAuthenticationFilter.java
@@ -1,4 +1,4 @@
-package org.example.config.filter;
+package org.example.filter;
 
 
 import jakarta.servlet.FilterChain;
